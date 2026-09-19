8be44d0ed43401757abae8986f9d9f735fcaade1
Gas Phaiboon <gusloveq@gmail.com>
2026-09-19 18:45:32 +0700

    Export express app for Vercel serverless functions

--- server/server.js
+++ server/server.js
@@ -44,6 +44,11 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
+
